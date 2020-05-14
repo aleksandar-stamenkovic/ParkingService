f0bc5fbab8f757e807e332624660fb64808ac91b
aleksandarelfak <aleksandarstamenkovic@elfak.rs>
2020-05-14 14:31:07 +0200

    Visual Studio projekat

---
+++ ParkingServis/ParkingServis/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ParkingServis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdUcitavanjeParkinga = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdUcitavanjeParkinga
+            // 
+            this.cmdUcitavanjeParkinga.Location = new System.Drawing.Point(13, 13);
+            this.cmdUcitavanjeParkinga.Name = "cmdUcitavanjeParkinga";
+            this.cmdUcitavanjeParkinga.Size = new System.Drawing.Size(132, 23);
+            this.cmdUcitavanjeParkinga.TabIndex = 0;
+            this.cmdUcitavanjeParkinga.Text = "Ucitavanje parkinga";
+            this.cmdUcitavanjeParkinga.UseVisualStyleBackColor = true;
+            this.cmdUcitavanjeParkinga.Click += new System.EventHandler(this.cmdUcitavanjeParkinga_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmdUcitavanjeParkinga);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdUcitavanjeParkinga;
+    }
+}
+
---
+++ ParkingServis/ParkingServis/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NHibernate;
+using ParkingServis.Entiteti;
+
+namespace ParkingServis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdUcitavanjeParkinga_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ISession s = DataLayer.GetSession();
+
+                Parking p = s.Load<Parking>(3);
+
+                MessageBox.Show(p.Naziv);
+
+                s.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
