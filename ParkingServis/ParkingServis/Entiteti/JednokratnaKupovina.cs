59d17ca81444f4df48b28f6f6bcc3173bc1dbd33
mixy98e <milan.mixy.stankovic@elfak.rs>
2020-05-14 21:23:31 +0200

    dodae jednokratna i pretplatnicka entiteti i map

---
+++ ParkingServis/ParkingServis/Entiteti/JednokratnaKupovina.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingServis.Entiteti
+{
+    public class JednokratnaKupovina
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime DatumProdaje { get; set; }
+        public virtual bool Iskoriscenost { get; set; }
+
+        //public virtual Vozilo PripadaVozilu { get; set; }
+    }
+}
---
+++ ParkingServis/ParkingServis/Mapiranja/PretplatnickaKupovinaMapiranja.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using ParkingServis.Entiteti;
+
+namespace ParkingServis.Mapiranja
+{
+    public class PretplatnickaKupovinaMapiranja : ClassMap<PretplatnickaKupovina>
+    {
+        public PretplatnickaKupovinaMapiranja()
+        {
+            // Mapiranje tabele
+            Table("PRETPLATNICKA_KUPOVINA");
+
+            // Mapiranje primarnog kljuca
+            Id(x => x.Id, "ID").GeneratedBy.TriggerIdentity();
+
+            // Mapiranje svojstava
+            Map(x => x.Zona, "ZONA");
+            Map(x => x.DatumProdaje, "DATUM_PRODAJE");
+            Map(x => x.PeriodVazenja, "PERIOD_VAZENJA");
+            Map(x => x.Iskoriscenost, "ISKORISCENOST");
+
+            //References(x => x.PripadaVozilu).Column("ID_VOZILA").LazyLoad();
+        }
+    }
+}
