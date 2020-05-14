ec28a4da206342357a2a1e35e6f6e9ee73bf71eb
mixy98e <milan.mixy.stankovic@elfak.rs>
2020-05-14 22:08:08 +0200

    entitet i mapiranje za zakup

---
+++ ParkingServis/ParkingServis/Entiteti/Zakup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingServis.Entiteti
+{
+    public class Zakup
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime Vreme { get; set; }
+        public virtual int PeriodZakupa { get; set; }
+        public virtual bool UlJavnoFleg { get; set; }
+
+        public virtual UlicnoMesto UlicnoMesto { get; set; }
+        public virtual JavnoMesto JavnoMesto { get; set; }
+        public virtual Vozilo Vozilo { get; set; }
+    }
+}
---
+++ ParkingServis/ParkingServis/Mapiranja/ZakupMapiranja.cs
@@ -0,0 +1,28 @@
+﻿using FluentNHibernate.Mapping;
+using ParkingServis.Entiteti;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingServis.Mapiranja
+{
+    public class ZakupMapiranja : ClassMap<Zakup>
+    {
+        public ZakupMapiranja()
+        {
+            Table("ZAKUP");
+
+            Id(x => x.Id, "ID").GeneratedBy.TriggerIdentity();
+
+            Map(x => x.Vreme, "VREME");
+            Map(x => x.PeriodZakupa, "PERIOD_ZAKUPA");
+            Map(x => x.UlJavnoFleg , "UL_JAVNO_FLEG");
+
+            References(x => x.UlicnoMesto).Column("ID_ULICNOG_MESTA").LazyLoad();
+            References(x => x.JavnoMesto).Column("ID_JAVNOG_MESTA").LazyLoad();
+            References(x => x.Vozilo).Column("ID_VOZILA").LazyLoad();
+        }
+    }
+}
